0766d7f8f2c224c073db079fb8000e6a1fefe7c2
wflin <funcprintf@gmail.com>
2021-12-31 22:57:53 +0800

    update TextViewExt.kt

--- library/src/main/java/com/wflin/ktext/TextViewExt.kt
+++ library/src/main/java/com/wflin/ktext/TextViewExt.kt
@@ -116,3 +116,10 @@ fun TextView.setDrawableLeft(drawable: Int) {
     this.setCompoundDrawablesWithIntrinsicBounds(drawable, 0, 0, 0)
 }
 
+/**
+ * Extension method to set a drawable to the right of TextView.
+ */
+fun TextView.setDrawableRight(drawable: Int) {
+    this.setCompoundDrawablesWithIntrinsicBounds(0, 0, drawable, 0)
+}
+
