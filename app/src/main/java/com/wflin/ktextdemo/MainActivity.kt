225079d69e6121215a4040e9b36d92f29b80e5e8
wflin <funcprintf@gmail.com>
2022-05-11 21:50:02 +0800

    add ViewPager2 sample

--- app/src/main/java/com/wflin/ktextdemo/MainActivity.kt
+++ app/src/main/java/com/wflin/ktextdemo/MainActivity.kt
@@ -2,10 +2,44 @@ package com.wflin.ktextdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.wflin.ktext.*
+import com.wflin.ktextdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+    }
+
+    private fun init() {
+        val fragList = mutableListOf<Fragment>()
+        // {@link ViewPager2Ext#init}, the same: ViewPager2.init(Fragment,MutableList<Fragment>, Boolean)
+        binding.vp2.init(this, fragList)
+        // back
+        binding.btnBack.click {
+            binding.vp2.back(true)
+        }
+        // next
+        binding.btnNext.click {
+            binding.vp2.next(false)
+        }
+        // setPageChangeCallback (the same: onPageScrollStateChanged, onPageScrolled, onPageSelected)
+        binding.vp2.setPageChangeCallback(
+            { state ->
+                // onStateChanged
+            },
+            { pos: Int, posOffset: Float, posOffsetPixels: Int ->
+                // onScrolled
+            },
+            { position ->
+                // onSelected
+            }
+        )
     }
 }
